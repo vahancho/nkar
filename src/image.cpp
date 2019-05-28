23619e648e72fe69d8fdf62c21ccd6297a62d537
Vahan Aghajanyan <vahancho@gmail.com>
2019-05-28 14:52:15 +0200

    Suspend compiler warnings of 3rd-party code.

--- src/image.cpp
+++ src/image.cpp
@@ -27,11 +27,13 @@
 #include "image.h"
 #include "point.h"
 
+#pragma warning(push, 0)
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+#pragma warning (pop)
 
 namespace nkar
 {
